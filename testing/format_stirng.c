358462e9a1104e4423ae20733013a663ea7bec44
Ted Meyer <tmathmeyer@gmail.com>
2014-11-13 15:45:16 -0500

    working on format stirng library for weather

---
+++ testing/format_stirng.c
@@ -0,0 +1,103 @@
+#include <string.h>
+#include <stdio.h>
+
+
+int main()
+{
+   /*
+        %T = temperature
+        $D = dew point
+        %H = relative humidity
+        %R = rainfall (last hour)
+        %P = pressure
+        %W = weather
+        %S = Sky Conditions
+      */
+
+    char dest[10] = {0};
+
+    printf("chars: %i\n", format_string(dest, "%%surprise"));
+
+    puts(dest);
+}
+
+char * get_source() {
+    char * string = "weather: something";
+}
+
+int temperature(int start, char * source, char * dest) {
+    return start;
+}
+
+int dew_point(int start, char * source, char * dest) {
+    return start;
+}
+
+int humidity(int start, char * source, char * dest) {
+    return start;
+}
+
+int rainfall(int start, char * source, char * dest) {
+    return start;
+}
+
+int pressure(int start, char * source, char * dest) {
+    return start;
+}
+
+int weather(int start, char * source, char * dest) {
+    return start;
+}
+
+int condition(int start, char * source, char * dest) {
+    return start;
+}
+
+
+int format_string(char * dest, char * fmt) {
+    int (*T)(int, char *, char *) = temperature;
+    int (*D)(int, char *, char *) = dew_point;
+    int (*H)(int, char *, char *) = humidity;
+    int (*R)(int, char *, char *) = rainfall;
+    int (*P)(int, char *, char *) = pressure;
+    int (*W)(int, char *, char *) = weather;
+    int (*S)(int, char *, char *) = condition;
+
+
+
+
+    char * source = get_source();
+    int e = 0;
+    int i = 0;
+    int d = 0;
+    for(;fmt[i];i++) {
+        if (fmt[i] == '%') {
+            d = !d;
+        } else if (d) {
+            switch(fmt[i]) {
+                case 'T':
+                    e = T(e, source, dest); break;
+                case 'D':
+                    e = D(e, source, dest); break;
+                case 'H':
+                    e = H(e, source, dest); break;
+                case 'R':
+                    e = R(e, source, dest); break;
+                case 'P':
+                    e = P(e, source, dest); break;
+                case 'W':
+                    e = W(e, source, dest); break;
+                case 'S':
+                    e = S(e, source, dest); break;
+                case '%':
+                    dest[e++] = '%'; break;
+                default:
+                    perror("FAIL");
+            }
+        } else {
+            dest[e++] = fmt[i];
+        }
+    }
+    return e;
+}
+
